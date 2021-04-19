58567726db5e6936a32474146e3b8c290d7b21e0
Ignacio Oyarzabal <ignaoya93@gmail.com>
2021-04-19 13:37:41 +0200

    setup whole page caching

--- students/urls.py
+++ students/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from . import views
 
 
@@ -17,5 +18,11 @@ urlpatterns = [
          name='student_course_detail'),
     path('course/<pk>/<module_id>/',
          views.StudentCourseDetailView.as_view(),
+         name='student_course_detail_module'),
+    path('course/<pk>/',
+         cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
+         name='student_course_detail'),
+    path('course/<pk>/<module_id>/',
+         cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
          name='student_course_detail_module'),
 ]
